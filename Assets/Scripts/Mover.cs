248a8c61aad2992f6dfa5fc81343b2b86ba3a2df
Rahul Soshte <rahul.soshte47@gmail.com>
2017-01-20 22:37:13 +0530

    Created Shots

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+	public float speed;
+	Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody>();
+		rigidbody.velocity = transform.forward * speed;
+
+	}
+
+}
